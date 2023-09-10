031a5c03b106d6091ac3ea5d55a9a8c0f740018b
RostSuslov <suslovros@gmail.com>
2023-09-10 17:07:34 +0300

    header done

--- vue-kalyna.design/src/components/Header.vue
+++ vue-kalyna.design/src/components/Header.vue
@@ -13,149 +13,120 @@
 </script>
 
 <template>
-    <header>
-        <div class="container">
-
-
-            <RouterLink terLink to="/" class="logo"><img src="src/assets/logo2.svg" alt="Logo" /></RouterLink>
-
-            <nav class="header-nav">
-
+    <header class="dark">
+        <nav class="bg-white border-gray-200 dark:border-gray-600 dark:bg-gray-900">
+            <div class="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
+                <RouterLink terLink to="/" class="logo"><img src="src/assets/logo2.svg" alt="Logo" /></RouterLink>
                 <button data-collapse-toggle="mega-menu-full" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="mega-menu-full" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
                     <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"></path>
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15"/>
                     </svg>
                 </button>
-                <div id="mega-menu-full" class="items-center justify-between w-full md:flex md:w-auto md:order-1 hidden">
-                    <ul class="nav-list">
-                        <li class="list-item">
-                            <RouterLink terLink to="/home">Home </RouterLink>
+                <div id="mega-menu-full" class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1">
+                    <ul class="flex flex-col mt-4 font-medium md:flex-row md:space-x-8 md:mt-0">
+                        <li>
+                            <RouterLink to="/home" class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Home</RouterLink>
                         </li>
-                        <li class="list-item">
-                            <button id="mega-menu-full-dropdown-button" class="dropdown-button" data-collapse-toggle="mega-menu-full-dropdown">
-                                Our articles
-                                <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-                                    fill="none" viewBox="0 0 10 6">
-                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="2" d="m1 1 4 4 4-4" />
+                        <li>
+                            <button id="mega-menu-full-dropdown-button" data-collapse-toggle="mega-menu-full-dropdown" class="flex items-center justify-between w-full py-2 pl-3 pr-4 font-medium text-gray-900 border-b border-gray-100 md:w-auto hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-600 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">
+                                Company 
+                                <svg class="w-2.5 h-2.5 ml-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
                                 </svg>
                             </button>
                         </li>
-                        <li class="list-item">
-                            <RouterLink to="/blog">Blog</RouterLink>
+                        <li>
+                            <RouterLink to="/blog" class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700" aria-current="page">Blog</RouterLink>
                         </li>
-                        <li class="list-item">
-                            <a href="Contact">Contact</a>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Resources</a>
+                        </li>
+                        <li>
+                            <a href="#" class="block py-2 pl-3 pr-4 text-gray-900 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-blue-500 md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
                         </li>
                     </ul>
                 </div>
-                <div id="mega-menu-full-dropdown" class="mega-menu-dropdown  hidden bg-white   shadow-sm border-y ">
+            </div>
+            <div id="mega-menu-full-dropdown" class="mt-1 hidden bg-white border-gray-200 shadow-sm border-y dark:bg-gray-800 dark:border-gray-600">
+                <div class="grid max-w-screen-xl px-4 py-5 mx-auto text-gray-900 dark:text-white sm:grid-cols-2 md:grid-cols-3 md:px-6">
                     <ul aria-labelledby="mega-menu-full-dropdown-button">
                         <li>
-                            <a href="#" class=" ">
-                                <div class=" ">Online Stores</div>
-                                <span class=" ">Connect with third-party tools that you're already using.</span>
+                            <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div class="font-semibold">Online Stores</div>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class=" ">
-                                <div class=" ">Segmentation</div>
-                                <span class=" ">Connect with third-party tools that you're already using.</span>
+                            <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div class="font-semibold">Segmentation</div>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class=" ">
-                                <div class=" ">Marketing CRM</div>
-                                <span class=" ">Connect with third-party tools that you're already using.</span>
+                            <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div class="font-semibold">Marketing CRM</div>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
                             </a>
                         </li>
                     </ul>
                     <ul>
                         <li>
-                            <a href="#" class=" ">
-                                <div class=" ">Online Stores</div>
-                                <span class=" ">Connect with third-party tools that you're already using.</span>
+                            <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div class="font-semibold">Online Stores</div>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class=" ">
-                                <div class=" ">Segmentation</div>
-                                <span class=" ">Connect with third-party tools that you're already using.</span>
+                            <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div class="font-semibold">Segmentation</div>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class=" ">
-                                <div class=" ">Marketing CRM</div>
-                                <span class=" ">Connect with third-party tools that you're already using.</span>
+                            <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div class="font-semibold">Marketing CRM</div>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
                             </a>
                         </li>
                     </ul>
                     <ul class="hidden md:block">
                         <li>
-                            <a href="#" class=" ">
-                                <div class=" ">Audience Management</div>
-                                <span class=" ">Connect with third-party tools that you're already using.</span>
+                            <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div class="font-semibold">Audience Management</div>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class=" ">
-                                <div class=" ">Creative Tools</div>
-                                <span class=" ">Connect with third-party tools that you're already using.</span>
+                            <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div class="font-semibold">Creative Tools</div>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
                             </a>
                         </li>
                         <li>
-                            <a href="#" class=" ">
-                                <div class=" ">Marketing Automation</div>
-                                <span class=" ">Connect with third-party tools that you're already using.</span>
+                            <a href="#" class="block p-3 rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700">
+                                <div class="font-semibold">Marketing Automation</div>
+                                <span class="text-sm text-gray-500 dark:text-gray-400">Connect with third-party tools that you're already using.</span>
                             </a>
                         </li>
                     </ul>
                 </div>
-            </nav>
-        </div>
+            </div>
+        </nav>
     </header>
-
 </template>
 
 <style scoped>
-    header {
-        padding: 24px 0;
-        background-color: var(--dark-bg);
-        color: #fff;
-        border-bottom: 1px solid #e9e9e9;
-    }
-
-    header .container {
-        display: flex;
-    }
-
     .logo {
         display: block;
         max-width: 200px;
         margin-right: 12px;
     }
 
-    .nav-list {
-        display: flex;
-        align-items: center;
-        padding: 0 12px;
-        margin-left: auto;
-    }
-
-    .nav-list a {
-        padding: 12px;
-        font-size: 16px;
-        font-weight: 600;
-    }
-
-    a:hover {
+  
+    #mega-menu-full a:hover,
+    #mega-menu-full button:hover {
         color: var(--primary-color);
     }
 
-
-    .dropdown-button {
-        display: flex;
-        align-items: center;
-    }
 </style>
